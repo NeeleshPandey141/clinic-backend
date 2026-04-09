cb2277d99093c6b4edd8497d38aca9f153e30294
Neelesh Pandey <pandeyneelesh59@gmail.com>
2026-04-09 20:01:07 +0530

    logs

--- server.js
+++ server.js
@@ -76,5 +76,11 @@ message:"Error saving appointment"
 
 });
 
-app.listen(5000,
-()=>console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+// app.listen(5000,
+// ()=>console.log("Server running on port 5000"));
